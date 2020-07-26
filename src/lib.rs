265c153d91c656f4a8786706ff3dab96ce85cc0c
Andrew Halle <ahalle@berkeley.edu>
2020-07-26 11:59:27 -0700

    add the QuantumComputer struct to match the API of the book.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use rand::prelude::*;
+use std::f64::consts::*;
+use std::mem::swap;
 
 #[derive(Debug, PartialEq)]
 pub enum Binary {
@@ -8,112 +10,118 @@ pub enum Binary {
 
 /// Representation of a qubit.
 #[derive(Debug, PartialEq)]
-pub struct Qubit(f64, f64);
+struct Qubit(f64, f64);
 
 impl Qubit {
-    pub fn zero() -> Self {
+    fn zero() -> Self {
         Qubit(1.0, 0.0)
     }
 
-    pub fn one() -> Self {
+    fn one() -> Self {
         Qubit(0.0, 1.0)
     }
 }
 
-/// Operations supported on qubits (both single-qubit operations and multi-qubit operations).
-pub mod ops {
-    use super::*;
-    use std::f64::consts::*;
-    use std::mem::swap;
+/// An quantum computer prepared with a single qubit.
+pub struct QuantumComputer {
+    q: Qubit,
+}
+
+impl QuantumComputer {
+    /// Initialize the quantum computer with a zero qubit.
+    pub fn reset(_n: u8) -> Self {
+        QuantumComputer { q: Qubit::zero() }
+    }
 
     /// Quantum NOT operator.
-    pub fn not(q: &mut Qubit) {
-        swap(&mut q.0, &mut q.1);
+    pub fn not(&mut self) {
+        swap(&mut self.q.0, &mut self.q.1);
     }
 
     /// Quantum HAD operator (Hadamard gate).
-    pub fn had(q: &mut Qubit) {
-        let tmp = q.0;
-        q.0 = FRAC_1_SQRT_2 * (q.0 + q.1);
-        q.1 = FRAC_1_SQRT_2 * (tmp - q.1);
+    pub fn had(&mut self) {
+        let tmp = self.q.0;
+        self.q.0 = FRAC_1_SQRT_2 * (self.q.0 + self.q.1);
+        self.q.1 = FRAC_1_SQRT_2 * (tmp - self.q.1);
     }
 
     /// Quantum READ operator. Returns a random result with probability based on the
     /// magnitudes.
-    pub fn read(q: &mut Qubit) -> Binary {
+    pub fn read(&mut self) -> Binary {
         let measurement: f64 = thread_rng().gen();
-        read_deterministic(q, measurement)
+        self.read_deterministic(measurement)
     }
 
-    fn read_deterministic(q: &mut Qubit, measurement: f64) -> Binary {
-        if measurement < q.0.powi(2) {
-            q.0 = 1.0;
-            q.1 = 0.0;
+    fn read_deterministic(&mut self, measurement: f64) -> Binary {
+        if measurement < self.q.0.powi(2) {
+            self.q.0 = 1.0;
+            self.q.1 = 0.0;
 
             Binary::Zero
         } else {
-            q.0 = 0.0;
-            q.1 = 1.0;
+            self.q.0 = 0.0;
+            self.q.1 = 1.0;
 
             Binary::One
         }
     }
 
     /// Quantum WRITE operator. Deterministically sets the values of a qubit.
-    pub fn write(target: &mut Qubit, value: Binary) {
+    pub fn write(&mut self, value: Binary) {
         let mut q = match value {
             Binary::Zero => Qubit::zero(),
             Binary::One => Qubit::one(),
         };
 
-        swap(target, &mut q);
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        #[test]
-        fn not() {
-            let mut q = Qubit::zero();
-            ops::not(&mut q);
-            assert_eq!(q, Qubit::one());
-        }
-
-        #[test]
-        fn had() {
-            let mut q = Qubit::zero();
-            ops::had(&mut q);
-            assert_eq!(q, Qubit(FRAC_1_SQRT_2, FRAC_1_SQRT_2));
-
-            let mut q = Qubit::one();
-            ops::had(&mut q);
-            assert_eq!(q, Qubit(FRAC_1_SQRT_2, -FRAC_1_SQRT_2));
-        }
-
-        #[test]
-        fn read_deterministic() {
-            let mut q = Qubit::zero();
-            ops::had(&mut q);
-            let res = ops::read_deterministic(&mut q, 0.49);
-            assert_eq!(res, Binary::Zero);
-
-            ops::had(&mut q);
-            let res = ops::read_deterministic(&mut q, 0.51);
-            assert_eq!(res, Binary::One);
-        }
-
-        #[test]
-        fn write() {
-            let mut q = Qubit::one();
-            ops::write(&mut q, Binary::Zero);
-            assert_eq!(q, Qubit::zero());
-        }
+        swap(&mut self.q, &mut q);
     }
 }
 
 #[cfg(test)]
-mod tests {
+mod qc_tests {
+    use super::*;
+
+    #[test]
+    fn not() {
+        let mut qc = QuantumComputer::reset(1);
+        qc.not();
+        assert_eq!(qc.q, Qubit::one());
+    }
+
+    #[test]
+    fn had() {
+        let mut qc = QuantumComputer::reset(1);
+        qc.had();
+        assert_eq!(qc.q, Qubit(FRAC_1_SQRT_2, FRAC_1_SQRT_2));
+
+        qc = QuantumComputer::reset(1);
+        qc.write(Binary::One);
+        qc.had();
+        assert_eq!(qc.q, Qubit(FRAC_1_SQRT_2, -FRAC_1_SQRT_2));
+    }
+
+    #[test]
+    fn read_deterministic() {
+        let mut qc = QuantumComputer::reset(1);
+        qc.had();
+        let res = qc.read_deterministic(0.49);
+        assert_eq!(res, Binary::Zero);
+
+        qc.had();
+        let res = qc.read_deterministic(0.51);
+        assert_eq!(res, Binary::One);
+    }
+
+    #[test]
+    fn write() {
+        let mut qc = QuantumComputer::reset(0);
+        qc.write(Binary::One);
+        assert_eq!(qc.q, Qubit::one());
+    }
+}
+
+#[cfg(test)]
+mod qubit_tests {
     use super::*;
 
     #[test]
