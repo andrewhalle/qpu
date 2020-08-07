60d2aca9ee205795ac26665c81128329e7319e75
Andrew Halle <ahalle@berkeley.edu>
2020-08-07 08:23:39 -0700

    filled out ex2_4

--- examples/ex2_4.rs
+++ examples/ex2_4.rs
@@ -1,41 +1,46 @@
 // Ex 2-4: Quantum Spy Hunter
-use qpu::{QuantumComputer, QubitTarget};
+use qpu::QuantumComputer;
+use rand::prelude::*;
 
-fn get_random_bit<T: Into<QubitTarget>>(qc: &mut QuantumComputer, target: T) -> usize {
-    let target = target.into();
-
-    qc.write(0, target);
-    qc.had(target);
-
-    qc.read(target)
+enum SimulationResult {
+    UnusableBit,
+    UsableBit,
+    SpyDetected,
 }
 
 // Runs the spy hunter protocol. Returns true if a spy is caught.
-fn run_spy_hunter_protocol() -> bool {
+fn send_one_qubit() -> SimulationResult {
+    let mut rng = thread_rng();
+
+    // -- simulation parameters
+    let alice_not = rng.gen_bool(0.5);
+    let alice_val = if alice_not { 1 } else { 0 };
+    let alice_had = rng.gen_bool(0.5);
+
+    // being a spy-hunter qubit means we send the value over the authenticated channel
+    // to try to detect a spy.
+    let is_spy_hunter = rng.gen_bool(0.01);
+    let spy_is_present = rng.gen_bool(0.5);
+    let spy_had = rng.gen_bool(0.5);
+
+    let bob_had = rng.gen_bool(0.5);
+
     let mut qc = QuantumComputer::reset(3);
 
     let a = qc.qint(1, "alice");
     let fiber = qc.qint(1, "fiber");
     let b = qc.qint(1, "bob");
 
-    qc.label("Generate two random bits.");
-    let send_had = get_random_bit(&mut qc, a);
-    let send_value = get_random_bit(&mut qc, a);
-
-    let spy_is_present = get_random_bit(&mut qc, fiber);
-
-    let recv_had = get_random_bit(&mut qc, b);
-
     // Prepare Alice's qubit.
     qc.write(0, a);
     qc.label("set value");
-    if send_value == 1 {
+    if alice_not {
         qc.not(a);
     }
     qc.end_label();
 
     qc.label("apply had");
-    if send_had == 1 {
+    if alice_had {
         qc.had(a);
     }
     qc.end_label();
@@ -44,9 +49,8 @@ fn run_spy_hunter_protocol() -> bool {
     qc.exchange(a, fiber);
 
     // Activate the spy
-    if spy_is_present == 1 {
+    if spy_is_present {
         qc.label("spy");
-        qc.had(fiber);
 
         let stolen_data = qc.read(fiber);
 
@@ -54,7 +58,10 @@ fn run_spy_hunter_protocol() -> bool {
         if stolen_data == 1 {
             qc.not(fiber);
         }
-        qc.had(fiber);
+
+        if spy_had {
+            qc.had(fiber);
+        }
         qc.end_label();
     }
 
@@ -62,38 +69,53 @@ fn run_spy_hunter_protocol() -> bool {
     qc.exchange(fiber, b);
 
     qc.label("apply had");
-    if recv_had == 1 {
+    if bob_had {
         qc.had(b);
     }
     qc.end_label();
 
     qc.label("read value");
-    let recv_val = qc.read(b);
+    let bob_val = qc.read(b);
     qc.end_label();
 
-    // Now Alice emails Bob to tell
-    // him her had setting and value.
-    // If the had setting matches and the
-    // value does not, there's a spy!
-    if send_had == recv_had {
-        if send_value != recv_val {
-            return true;
+    // basis setting is always shared
+    if alice_had == bob_had {
+        // only check values if this is a spy-hunter qubit, otherwise we don't share
+        // the value
+        if is_spy_hunter && alice_val != bob_val {
+            return SimulationResult::SpyDetected;
+        } else if is_spy_hunter {
+            // spy hunter qubits aren't usable because we share the value over a public channel.
+            return SimulationResult::UnusableBit;
+        } else {
+            return SimulationResult::UsableBit;
         }
     }
 
-    false
+    SimulationResult::UnusableBit
 }
 
 fn main() {
+    let mut usable = 0;
     let mut caught = 0;
     let mut total = 0;
+
     while total < 1000 {
-        if run_spy_hunter_protocol() {
-            caught += 1;
+        match send_one_qubit() {
+            SimulationResult::UnusableBit => {}
+            SimulationResult::UsableBit => usable += 1,
+            SimulationResult::SpyDetected => {
+                caught += 1;
+                break;
+            }
         }
 
         total += 1;
     }
 
-    println!("Caught {} spies in {} trials.", caught, total);
+    if caught != 0 {
+        println!("Caught a spy!");
+    } else {
+        println!("{} usable bits extracted out of {}", usable, total);
+    }
 }
